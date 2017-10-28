4c3cbc8d58991f8d9e18a65d2b0939012995d6f8
Maxim Neverov <neverov.max@gmail.com>
2017-10-28 17:58:32 +0300

    Added repositories to newsletter service

--- src/main/java/cc/service/NewsletterServiceImpl.java
+++ src/main/java/cc/service/NewsletterServiceImpl.java
@@ -1,9 +1,14 @@
 package cc.service;
 
+import cc.common.model.CategoryNode;
 import cc.common.model.Newsletter;
+import cc.common.model.Subscriber;
+import cc.persistence.book.BookRepository;
+import cc.persistence.subscriber.SubscriberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.inject.Inject;
 import java.util.Collections;
 import java.util.List;
 
@@ -14,8 +19,23 @@ import java.util.List;
 @Service
 public class NewsletterServiceImpl implements NewsletterService {
 
+    private final List<CategoryNode> categories;
+    private final SubscriberRepository subscriberRepository;
+    private final BookRepository bookRepository;
+
+    @Inject
+    public NewsletterServiceImpl(List<CategoryNode> categories,
+                                 SubscriberRepository subscriberRepository,
+                                 BookRepository bookRepository) {
+        this.categories = categories;
+        this.subscriberRepository = subscriberRepository;
+        this.bookRepository = bookRepository;
+    }
+
     @Override
     public List<Newsletter> getNewsletters() {
+        List<Subscriber> subscribers = subscriberRepository.getSubscribers();
+
         return Collections.emptyList();
     }
 
