877e4dae82140cc149599e8c6965a44594870e2f
Maxim Neverov <neverov.max@gmail.com>
2017-10-28 09:00:49 +0300

    Added subscriber controller

---
+++ src/main/java/cc/rest/SubscriberController.java
@@ -0,0 +1,26 @@
+package cc.rest;
+
+import cc.rest.model.Subscriber;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author Maxim Neverov
+ */
+@Slf4j
+@Controller
+@RequestMapping("/subscribers")
+public class SubscriberController {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void submitSubscriber(@RequestBody Subscriber subscriber) {
+        log.info("Got the subscriber {}", subscriber);
+    }
+
+}
