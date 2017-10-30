d59e61c18b17bc033c4c7abc3396d371dbb501a3
Maxim Neverov <neverov.max@gmail.com>
2017-10-30 08:45:55 +0300

    Implemented book repository

--- src/main/java/cc/persistence/book/BookRepository.java
+++ src/main/java/cc/persistence/book/BookRepository.java
@@ -1,7 +1,12 @@
 package cc.persistence.book;
 
+import cc.persistence.dto.BookDto;
+
 /**
  * @author Maxim Neverov
  */
 public interface BookRepository {
+
+    void create(BookDto book);
+
 }
--- src/main/java/cc/persistence/book/BookRepositoryImpl.java
+++ src/main/java/cc/persistence/book/BookRepositoryImpl.java
@@ -1,7 +1,34 @@
 package cc.persistence.book;
 
+import cc.persistence.dto.BookDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
 /**
  * @author Maxim Neverov
  */
+@Slf4j
+@Repository
 public class BookRepositoryImpl implements BookRepository {
+
+    private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
+
+    @Inject
+    public BookRepositoryImpl(DataSource dataSource, JdbcTemplate jdbcTemplate) {
+        this.dataSource = dataSource;
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void create(BookDto book) {
+        String SQL = "insert into BOOK (title, ) values (?, ?)";
+        jdbcTemplate.update( SQL, book.getTitle(), book.getCategoryCodes());
+        log.info("Book {} created", book);
+    }
+
 }
