f8b89f822a86d03f40b5cf10e25877a9c3df0dbf
Maxim Neverov <neverov.max@gmail.com>
2017-10-31 23:14:29 +0300

    Implemented subscriber saving, retrieving

--- src/main/java/cc/persistence/subscriber/SubscriberRepositoryImpl.java
+++ src/main/java/cc/persistence/subscriber/SubscriberRepositoryImpl.java
@@ -1,10 +1,15 @@
 package cc.persistence.subscriber;
 
-import cc.common.model.Subscriber;
+import cc.persistence.dto.SubscriberDto;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collections;
+import javax.inject.Inject;
+import javax.sql.DataSource;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Maxim Neverov
@@ -12,15 +17,38 @@ import java.util.List;
 @Repository
 public class SubscriberRepositoryImpl implements SubscriberRepository {
 
+    private final DataSource dataSource;
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+    private final SubscriberRowMapper mapper;
+
+    @Inject
+    public SubscriberRepositoryImpl(DataSource dataSource,
+                                    NamedParameterJdbcTemplate jdbcTemplate,
+                                    SubscriberRowMapper mapper) {
+        this.dataSource = dataSource;
+        this.jdbcTemplate = jdbcTemplate;
+        this.mapper = mapper;
+    }
+
+    @SuppressWarnings("unchecked")
     @Override
-    public List<Subscriber> getSubscribers() {
-        return Collections.emptyList();
+    public void create(List<SubscriberDto> subscribers) {
+        String sql = "insert into subscriber (email, category_code) values (:email, :categoryCode)";
+        List<Map<String, Object>> batchValues = new ArrayList<>(subscribers.size());
+
+        for (SubscriberDto subscriber : subscribers) {
+            batchValues.add(new MapSqlParameterSource("email", subscriber.getEmail())
+                    .addValue("categoryCode", subscriber.getCategoryCode())
+                    .getValues());
+        }
+
+        jdbcTemplate.batchUpdate(sql, batchValues.toArray(new Map[subscribers.size()]));
     }
 
     @Override
-    public void saveSubscriber(Subscriber subscriber) {
-        // todo Neverov: implement
+    public List<SubscriberDto> getSubscribers() {
+        String sql = "select * from subscriber";
+        return jdbcTemplate.query(sql, mapper);
     }
-
 
 }
--- src/main/java/cc/rest/SubscriberController.java
+++ src/main/java/cc/rest/SubscriberController.java
@@ -1,6 +1,7 @@
 package cc.rest;
 
 import cc.common.model.Subscriber;
+import cc.service.subscriber.SubscriberService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.inject.Inject;
 
 /**
  * @author Maxim Neverov
@@ -17,10 +20,18 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @RequestMapping("/subscribers")
 public class SubscriberController {
 
+    private final SubscriberService service;
+
+    @Inject
+    public SubscriberController(SubscriberService service) {
+        this.service = service;
+    }
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void submitSubscriber(@RequestBody Subscriber subscriber) {
         log.info("Got the subscriber {}", subscriber);
+        service.create(subscriber);
     }
 
 }
