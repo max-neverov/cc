61e203e501d0222d57bec49008547effd9627872
Maxim Neverov <neverov.max@gmail.com>
2017-11-01 20:48:34 +0300

    Added test on successful book creation

---
+++ src/test/java/cc/rest/BookControllerTest.java
@@ -0,0 +1,63 @@
+package cc.rest;
+
+import cc.Application;
+import cc.common.model.Book;
+import cc.service.book.BookService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.Arrays;
+
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.reset;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+/**
+ * @author Maxim Neverov
+ */
+@ActiveProfiles("test")
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = Application.class)
+@WebAppConfiguration
+public class BookControllerTest {
+
+    private String PATH = "/books";
+    private MockMvc mockMvc;
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private BookService service;
+
+    @Before
+    public void setup() {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+        reset(service);
+    }
+
+    @Test
+    public void return201WhenCreated() throws Exception {
+        Book book = new Book("title", Arrays.asList("cat1", "cat2"));
+        doNothing().when(service).create(book);
+
+        mockMvc.perform(post(PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(book)))
+                .andExpect(status().isCreated());
+    }
+
+}
