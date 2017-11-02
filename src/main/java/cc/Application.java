82fa2298db9b333a0f0177a27a0b0c40c37037bd
Maxim Neverov <neverov.max@gmail.com>
2017-11-02 19:45:35 +0300

    Added swagger. Doesn't work - empty page

--- src/main/java/cc/Application.java
+++ src/main/java/cc/Application.java
@@ -3,12 +3,22 @@ package cc;
 import cc.rest.util.LoggingInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author Maxim Neverov
  */
+@EnableSwagger2
 @SpringBootApplication
 public class Application extends WebMvcConfigurerAdapter {
 
@@ -19,6 +29,30 @@ public class Application extends WebMvcConfigurerAdapter {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoggingInterceptor()).addPathPatterns("/**");
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .pathMapping("/")
+                .apiInfo(getApiInfo());
+    }
+
+    @SuppressWarnings("unchecked")
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Newsletter service")
+                .description("This service shows how to use different frameworks to deal with books subscriptions.")
+                .termsOfServiceUrl("urn:tos")
+                .contact(new Contact("Maxim Neverov", "https://github.com/max-neverov", "neverov.max@gmail.com"))
+                .license("Apache License Version 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0")
+                .version("1.0")
+                .build();
     }
 
 }
