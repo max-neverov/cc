255c0ca8646cc86f3f551fd380bd86c92425a572
Maxim Neverov <neverov.max@gmail.com>
2018-01-27 22:01:40 +0300

    Minor code improvement

--- src/main/resources/db/changelog/insert_data.sql
+++ src/main/resources/db/changelog/insert_data.sql
@@ -1,16 +1,16 @@
-insert into Category (code, title) values('cat1', 'title1');
-insert into Category (code, title) values('cat7', 'title7');
+insert into category (code, title) values('cat1', 'title1');
+insert into category (code, title) values('cat7', 'title7');
 
-insert into Category (code, title, super_category_code) values('cat2', 'title2', 'cat1');
-insert into Category (code, title, super_category_code) values('cat3', 'title3', 'cat2');
-insert into Category (code, title, super_category_code) values('cat4', 'title4', 'cat2');
-insert into Category (code, title, super_category_code) values('cat5', 'title5', 'cat2');
-insert into Category (code, title, super_category_code) values('cat6', 'title6', 'cat1');
-insert into Category (code, title, super_category_code) values('cat8', 'title8', 'cat7');
-insert into Category (code, title, super_category_code) values('cat9', 'title9', 'cat7');
-insert into Category (code, title, super_category_code) values('cat10', 'title10', 'cat8');
-insert into Category (code, title, super_category_code) values('cat11', 'title11', 'cat9');
-insert into Category (code, title, super_category_code) values('cat12', 'title12', 'cat9');
+insert into category (code, title, super_category_code) values('cat2', 'title2', 'cat1');
+insert into category (code, title, super_category_code) values('cat3', 'title3', 'cat2');
+insert into category (code, title, super_category_code) values('cat4', 'title4', 'cat2');
+insert into category (code, title, super_category_code) values('cat5', 'title5', 'cat2');
+insert into category (code, title, super_category_code) values('cat6', 'title6', 'cat1');
+insert into category (code, title, super_category_code) values('cat8', 'title8', 'cat7');
+insert into category (code, title, super_category_code) values('cat9', 'title9', 'cat7');
+insert into category (code, title, super_category_code) values('cat10', 'title10', 'cat8');
+insert into category (code, title, super_category_code) values('cat11', 'title11', 'cat9');
+insert into category (code, title, super_category_code) values('cat12', 'title12', 'cat9');
 
 
 insert into book (title, category_code) values('book1', 'cat3');
